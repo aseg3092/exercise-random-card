07ae298c06029bc6a0d2376275bf14a5d7a92307
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-18 01:24:32 +0000

    html, un poco de css y js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let elem = document.querySelectorAll(".palo");
+  let palo = palos[Math.floor(Math.random() * (4 - 0) + 0)];
+  for (let index = 0; index < elem.length; index++) {
+    elem[index].innerHTML = palo;
+  }
+
+  let numero = numeros[Math.floor(Math.random() * (13 - 0) + 0)];
+
+  document.querySelector(".numero").innerHTML = numero;
 };
